42b38e4e6ff9ada2599051a8e675ce7e570b5d34
aregret01 <aregret01@gmail.com>
2021-05-13 20:45:16 +0530

    add dummy values for views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+#dummy data for understanding templates and views
+
+posts = [
+    {
+        'author':'Akash',
+        'title':'Blog Post 1',
+        'content':'First Blog Content',
+        'data_publish':'17 March 2001',
+    },
+    {
+        'author':'Soumya',
+        'title':'Blog Post 2',
+        'content':'second Blog Content',
+        'data_publish':'14 January 1979',
+    },
+]
+def homePage(request):
+    context = {
+
+        'posts': posts 
+    }
+    
+    return render(request,'blog/index.html',context)
+
+def aboutPage(request):
+    return render(request,'blog/about.html',{'title':'About'})
