ca81e1a06ea1497a2a4f5a5232d9ae1f75ddea1d
aregret01 <aregret01@gmail.com>
2021-05-13 22:59:37 +0530

    create database and models

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 
 #dummy data for understanding templates and views
-
+'''
 posts = [
     {
         'author':'Akash',
         'title':'Blog Post 1',
         'content':'First Blog Content',
-        'data_publish':'17 March 2001',
+        'post_posted':'17 March 2001',
     },
     {
         'author':'Soumya',
         'title':'Blog Post 2',
         'content':'Second Blog Content',
-        'data_publish':'14 January 1979',
+        'post_posted':'14 January 1979',
     },
-]
+]'''
+
+
 def homePage(request):
     context = {
-
-        'posts': posts 
+        'posts': Post.objects.all() 
     }
     
     return render(request,'blog/index.html',context)
