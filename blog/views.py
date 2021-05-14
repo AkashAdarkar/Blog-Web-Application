25910bf1c751c7d96277c2c40e990b9512476387
aregret01 <aregret01@gmail.com>
2021-05-14 16:27:35 +0530

    added pagination for site

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -7,7 +8,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from .models import Post
 
 
@@ -24,7 +25,7 @@ posts = [
         'author':'Soumya',
         'title':'Blog Post 2',
         'content':'Second Blog Content',
-        'post_posted':'14 January 1979',
+        'post_posted':'14 January 2014',
     },
 ]'''
 
@@ -36,17 +37,34 @@ def homePage(request):
     
     return render(request,'blog/index.html',context)
 
-
 class PostListView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
     ordering = ['-post_posted']
+    paginate_by = 5
 
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' 
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-post_posted')
+
+
+
+
+
+#Posts
 class PostDetailView(DetailView):
     model = Post
 
+
+#create Post
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title','content']
@@ -55,6 +73,8 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+#Update Post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -70,6 +90,8 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return False
 
 
+
+#Delete Post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
